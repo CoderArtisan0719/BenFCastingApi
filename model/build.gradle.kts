df912fd5055ac87c678bf41ee630eb85d4f3d4ad
Sander de Boer <sander@sb-space.nl>
2024-06-12 13:08:49 +0200

    Setup Model Module

---
+++ model/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "nl.benfcasting.api"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
