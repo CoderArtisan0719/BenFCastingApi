53a963cfd320896b7d905159b5fb44c2b292c12f
Sander de Boer <sander@sb-space.nl>
2024-06-12 13:53:26 +0200

    Update Kotlin version, add Kotlin plugins, change build.gradle to .kts, refactor dependencies in build.gradle.kts, configure Kotlin compiler options and JUnit platform for tests. Delete and rename files accordingly.

---
+++ api/src/test/kotlin/nl/benfcasting/api/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.benfcasting.api.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
